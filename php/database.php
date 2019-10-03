87655fa09511a374de3e631f8779b8966b7c9593
Francois <francois.al-haddad-siderikoudis@etu.univ-amu.fr>
2019-10-03 15:22:24 +0200

    Fin journée - acces la base de donnée amelioration de la classe database

--- php/database.php
+++ php/database.php
@@ -5,18 +5,21 @@ class database {
     private $_user;
     private $_password;
     private $_dbLink;
+
     function __construct($host,$user,$password,$dbName)
     {
         $this->_host = $host;
         $this->_user = $user;
         $this->_password = $password;
-        $_dbLink = mysqli_connect($host, $user, $password)
+        $this->_dbLink = mysqli_connect($host, $user, $password)
         or die('Erreur de connexion au serveur : ' . mysqli_connect_error());
-        mysqli_select_db($_dbLink, $dbName)
-        or die('Erreur dans la sélection de la base : ' . mysqli_error($_dbLink)
+        mysqli_select_db($this->_dbLink, $dbName)
+        or die('Erreur dans la sélection de la base : ' . mysqli_error($this->_dbLink)
         );
     }
-    function CheckError($query){
+
+    function CheckError($query,$name,$name2,$name3,$name4,$name5,$name6){
+
         if(!($dbResult = mysqli_query($this->_dbLink, $query)))
         {
             echo 'Erreur de requête' . '<br/><br/>';
@@ -26,7 +29,6 @@ class database {
             echo 'Requête : ' . $query . '<br/>';
             exit();
         }
-//echo $query;
         if(!($dbResult = mysqli_query($this->_dbLink, $query)))
         {
             echo 'Erreur de requête<br/>';
@@ -35,18 +37,48 @@ class database {
             // Affiche la requête envoyée.
             echo 'Requête : ' . $query . '<br/>';
             exit();
+        } else {
+            while ($row = mysqli_fetch_assoc($dbResult)) {
+                  echo $row['\'' . $name . '\''] . '<br>';
+                  if($name2!=''){ echo $row['\'' . $name2 . '\''] . '<br>'; }
+                  if($name3!=''){ echo $row['\'' . $name3 . '\''] . '<br>'; }
+                  if($name4!=''){ echo $row['\'' . $name4 . '\''] . '<br>'; }
+                  if($name5!=''){ echo $row['\'' . $name5 . '\''] . '<br>'; }
+                  if($name6!=''){ echo $row['\'' . $name6 . '\''] . '<br>'; }
+//                echo $row['password'] . '<br><hr>';
+            }
         }
     }
 }
 
 
-$dbLink = mysqli_connect('mysql-francois.alwaysdata.net', 'francois_oui', '0621013579')
-or die('Erreur de connexion au serveur : ' . mysqli_connect_error());
-mysqli_select_db($dbLink, 'francois_bd1')
-or die('Erreur dans la sélection de la base : ' . mysqli_error($dbLink)
-);
-$query = 'SELECT id,email,password FROM user';
-echo $query;
+//$dbLink = mysqli_connect('mysql-baptistesevilla.alwaysdata.net', '189826_admin1', '0651196362')
+//or die('Erreur de connexion au serveur : ' . mysqli_connect_error());
+//mysqli_select_db($dbLink, 'francois_bd1');
+////or die('Erreur dans la sélection de la base : ' . mysqli_error($dbLink)
+//$query = 'SELECT * FROM User';
+////echo $query;
+
+$databaseBaptiste = new database('mysql-baptistesevilla.alwaysdata.net','189826_admin1','0651196362','baptistesevilla_projetweb');
+$databaseBaptiste->CheckError('Select Script from Topics',Script);
+
+//if(!($dbResult = mysqli_query($dbLink, $query)))
+//{
+////    echo 'Erreur de requête<br/>';
+//    // Affiche le type d'erreur.
+////    echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
+//    // Affiche la requête envoyée.
+//    echo 'Requête : ' . $query . '<br/>';
+//    exit();
+//} else {
+//    while ($row = mysqli_fetch_assoc($dbResult)) {
+//        echo $row['id'] . '<br>';
+////        echo $row['email'] . '<br>';
+////        echo $row['password'] . '<br><hr>';
+//    }
+//}
+
+
 //
 //$database1 = new database('mysql-baptistesevilla.alwaysdata.net','189826_admin1','0621013579','User');
 //$database1->CheckError()//
