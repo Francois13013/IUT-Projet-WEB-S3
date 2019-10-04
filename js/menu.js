7251a9801b9ea49605ed28a7ad0ae845ba01ffa8
François <Francois.al-haddad-siderikoudis@etu.univ-amu.fr>
2019-10-04 17:51:35 +0200

    Amelioration du menu hamburger CSS & HTML Amelioration du menu connection CSS & HTML Correction de bug JS notament celui des deux menus Mis en place d'un système en js qui evite à l'utilisateur de supprimer la value d'une input. Réajustement mineurs de la page en html.

--- js/menu.js
+++ js/menu.js
@@ -3,13 +3,43 @@ LogoUser = document.getElementById('user');
 menuHamburger = document.querySelector('.menuphonehamburger');
 menuUser = document.querySelector('.menuphoneuser');
 
+inputusername = document.getElementById('usernameinput');
+inputpassword = document.getElementById('passwordinput');
+
+inputpassword.onclick = function (){
+    // alert(inputpassword.getAttribute('value'));
+    if(inputpassword.getAttribute('value') == 'password'){
+        // alert("test");
+        inputpassword.setAttribute('value', '');
+    }
+    //!= 'password') {
+    //
+    // }
+}
+
+inputusername.onclick = function (){
+    if(inputusername.getAttribute('value') == 'username'){
+        inputusername.setAttribute('value', '');
+    }
+}
+
 LogoHamburger.onclick = function(){
+    timetmp = menuUser.style.transitionDuration;
+    menuUser.style.transitionDuration = '0s';
+    menuUser.classList.remove('offmenu');
     menuHamburger.classList.toggle("offmenu");
+    menuUser.style.transitionDuration = timetmp.toString();
 }
 
 LogoUser.onclick = function(){
+    timetmp = menuHamburger.style.transitionDuration;
+    menuHamburger.style.transitionDuration = '0s';
+    menuHamburger.classList.remove('offmenu');
     menuUser.classList.toggle("offmenu");
+    menuHamburger.style.transitionDuration = timetmp.toString();
 }
+
+
 
 // window.onclick =function (){
 //     alert('100');
