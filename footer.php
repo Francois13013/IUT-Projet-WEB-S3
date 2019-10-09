4edcad8d5d132cf57dba3e17cc44600cac574ff0
p18011704 <corentin.plee@etu.univ-etu.fr>
2019-10-09 17:17:04 +0200

    Footer

--- footer.php
+++ footer.php
@@ -2,7 +2,10 @@
 <link rel="stylesheet" href="css/stylefooter.css">
 <div>
     <footer id="footer">
-        <p>Ce site a été réaliser par un groupe d'étudiant de l'Iut d'informatique d'Aix-Marseille composé de :.... .... ..... ..... ...... ...... ...... .....</p>
+        <ul class="list-style-none">
+                <p>©2019
+                Ce site a été réaliser par un groupe d'étudiant de l'Iut d'informatique d'Aix-Marseille composé de :.... .... ..... ..... ...... ...... ...... ....
+                </p>
     </footer>
 </div>
 <script src="js/menu.js"></script>
--- index.php
+++ index.php
@@ -1,7 +1,3 @@
 <?php include_once('header.php')?>
-hzaiouphzadoiuzhdiouzadhziuda
-<br>
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>dzadzazdadaz<br><br><br><br><br>
-
 <?php include_once('footer.php')?>
 
