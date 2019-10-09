6dfdb4583cc27c63b8ba19823d81fbde4fa54751
Francois <francois.al-haddad-siderikoudis@etu.univ-amu.fr>
2019-10-09 16:46:13 +0200

    Footer fonctionnel

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php include_once('header.php')?>
-hzaiouphzadoiuzhdiouzadhziuda
-<br>
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>dzadzazdadaz<br><br><br><br><br>
-
-<?php include_once('footer.php')?>
-
