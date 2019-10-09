2442ab0b24db7ece0d5d049b98e8e1a1c83b1be0
p18011704 <corentin.plee@etu.univ-etu.fr>
2019-10-09 16:12:45 +0200

    Footer

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 <link rel="stylesheet" href="css/stylefooter.css">
 <div>
     <footer id="footer">
-        <p>Ce site a été réaliser par un groupe d'étudiant de l'Iut d'informatique d'Aix-Marseille composé de :..... .... ..... ..... ...... ...... ...... .....</p>
+        <p>Ce site a été réaliser par un groupe d'étudiant de l'Iut d'informatique d'Aix-Marseille composé de :.... .... ..... ..... ...... ...... ...... .....</p>
     </footer>
 </div>
 <script src="js/menu.js"></script>
