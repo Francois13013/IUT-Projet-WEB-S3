be489f756bfaed1a7206e6345156c7170b6b8dc5
p18011704 <corentin.plee@etu.univ-etu.fr>
2019-10-09 12:06:23 +0200

    COUCOU

--- footer.php
+++ footer.php
@@ -1,4 +1,10 @@
 </div>
+<link rel="stylesheet" href="css/stylefooter.css">
+<div>
+    <footer id="footer">
+        <p>Ce site a été réaliser par un groupe d'étudiant de l'Iut d'informatique d'Aix-Marseille composé de :..... .... ..... ..... ...... ...... ...... .....</p>
+    </footer>
+</div>
 <script src="js/menu.js"></script>
 
 <script src="js/logged.js"></script>
