1aae4f184493ac400db8bd2c7035e951beb4135e
François <Francois.al-haddad-siderikoudis@etu.univ-amu.fr>
2019-10-12 11:36:20 +0200

    CreateView

--- Routes.php
+++ Routes.php
@@ -1,9 +1,9 @@
 <?php
 require_once('classes/Route.php');
+require_once('Controllers/Controller.php');
 
 Router::add('/testa', function() {
-    require ('header.php');
-    include_once('footer.php');
+    Controller::CreateView('index');
 });
 
 //$test = new Router();
