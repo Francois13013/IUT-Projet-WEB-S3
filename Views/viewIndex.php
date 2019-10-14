f2015e50ec35d7f9e4a8ada733dd49e70625cfe1
François <Francois.al-haddad-siderikoudis@etu.univ-amu.fr>
2019-10-14 21:23:12 +0200

    Refonte Accueil

--- Views/viewIndex.php
+++ Views/viewIndex.php
@@ -1,5 +1,16 @@
-
+<div id="mainIndex">
+<link rel="stylesheet" href="../Public/css/styleIndex.css">
 <h1>Bievenue sur l'accueil</h1>
+<div class="container" id="containerTopic">
+    <div class="topicRow"></div>
+    <div class="topicRow"></div>
+    <div class="topicRow"></div>
+    <div class="topicRow"></div>
+    <div class="topicRow"></div>
+</div>
+<div class="container" id="containerInfo">
+</div>
+</div>
 <?php
 
 ?>
