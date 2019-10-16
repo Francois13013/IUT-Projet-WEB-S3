efe90a791118a274e21a3c38823643c0489207d9
r18020152 <florent.reymond@etu.univ-amu.fr>
2019-10-16 11:26:51 +0200

    Myprofile

--- Views/viewMyProfile.php
+++ Views/viewMyProfile.php
@@ -4,11 +4,23 @@
 //    echo $User->getFirstName();
 
 
-
-
 echo $_SESSION["IdUser"] . '<br><hr>';
 echo $_SESSION["Surname"] . '<br><hr>';
 echo $_SESSION["Email"] . '<br><hr>';
 echo $_SESSION["Password"] . '<br><hr>';
 echo $_SESSION["Status"] . '<br><hr>';
+
 ?>
+<?php
+echo 'Change Surname' . '<br>';
+echo $_SESSION["Surname"] . '<br><hr>';
+echo 'Change Email' . '<br>';
+echo $_SESSION["Email"] . '<br><hr>';
+echo 'Change Password' . '<br>';
+echo $_SESSION["Password"] . '<br><hr>';
+echo $_SESSION["Status"] . '<br><hr>';
+
+?>
+
+
+
