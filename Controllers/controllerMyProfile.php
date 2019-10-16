25b72c33bf10de3d7677dc0b8c3d534fecf5556d
r18020152 <florent.reymond@etu.univ-amu.fr>
2019-10-16 11:39:20 +0200

    Myprofile

--- Controllers/controllerMyProfile.php
+++ Controllers/controllerMyProfile.php
@@ -1,3 +1,8 @@
 <?php
+require_once('../classes/Database.php');
+session_start();
+
+$email = $_POST['email'];
+$databaseBaptiste = new database('mysql-baptistesevilla.alwaysdata.net', '189826_admin1', '0651196362', 'baptistesevilla_projetweb');
 
 ?>
--- Views/viewMyProfile.php
+++ Views/viewMyProfile.php
@@ -11,14 +11,21 @@ echo $_SESSION["Password"] . '<br><hr>';
 echo $_SESSION["Status"] . '<br><hr>';
 
 ?>
+
+<button></button>
+
 <?php
 echo 'Change Surname' . '<br>';
-echo $_SESSION["Surname"] . '<br><hr>';
+?>
+<input class="favorite styled"
+       type="button"
+       value="Change Surname">.<br>;
+
+<?php
 echo 'Change Email' . '<br>';
 echo $_SESSION["Email"] . '<br><hr>';
 echo 'Change Password' . '<br>';
 echo $_SESSION["Password"] . '<br><hr>';
-echo $_SESSION["Status"] . '<br><hr>';
 
 ?>
 
