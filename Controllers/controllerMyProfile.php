2787b26f2f7d970498dd418f10052c19abceb1d8
FlorentRmd <florent.reymond@etu.univ-amu.fr>
2019-10-18 21:59:44 +0200

    Gestion profil

--- Controllers/controllerMyProfile.php
+++ Controllers/controllerMyProfile.php
@@ -1,8 +1,21 @@
 <?php
+require_once('../classes/User.php');
 require_once('../classes/Database.php');
 session_start();
 
-$email = $_POST['email'];
+
+$password = $_POST['hachek'] ;
+$user = $_SESSION['user'];
+//echo 'Change Mot de passe ';
+//print_r($_SESSION['user']);
+
+
+echo $password;
+print_r($user);
+
+$user->SetPassword(454545);
+
 $databaseBaptiste = new database('mysql-baptistesevilla.alwaysdata.net', '189826_admin1', '0651196362', 'baptistesevilla_projetweb');
+$databaseBaptiste->insertNewPassword($password);
 
 ?>
--- Views/viewMyProfile.php
+++ Views/viewMyProfile.php
@@ -11,15 +11,19 @@ echo $_SESSION["Password"] . '<br><hr>';
 echo $_SESSION["Status"] . '<br><hr>';
 
 ?>
+<?php;
+if ($_SESSION["Status"] == 1)
+//        echo $_SESSION["Status"] . '<br><hr>';
+    echo '<a class="aligntext">' . "admin" . '</a>';
+elseif ($_SESSION["Status"] == 2)
+    echo '<a class="aligntext">' . "utilisateur". '</a>';
+?>
 
-<button></button>
 
 <?php
 echo 'Change Surname' . '<br>';
 ?>
-<input class="favorite styled"
-       type="button"
-       value="Change Surname">.<br>;
+<input class="favorite styled" type="button" value="Change Surname">. <br>;
 
 <?php
 echo 'Change Email' . '<br>';
@@ -27,7 +31,15 @@ echo $_SESSION["Email"] . '<br><hr>';
 echo 'Change Password' . '<br>';
 echo $_SESSION["Password"] . '<br><hr>';
 
+
 ?>
+<form method="post" action="../Controllers/controllerMyProfile.php">
+    <label>zeub</label>
+    <input class="favorite styled" type="text" name="hachek">;
+    <input class="favorite styled" type="submit" value="action">;
+
+</form>
+
 
 
 
