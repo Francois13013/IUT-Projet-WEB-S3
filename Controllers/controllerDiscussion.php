3323bec47fe45ba6940f601781a83b3362deaa2e
François <Francois.al-haddad-siderikoudis@etu.univ-amu.fr>
2019-10-26 10:13:22 +0200

    Verification des deux mots

--- Controllers/controllerDiscussion.php
+++ Controllers/controllerDiscussion.php
@@ -24,24 +24,30 @@ function RequestMessages(){
     }
 }
 
-function AddWords(){
+function AddWords()
+{
+    $messageToSend = $_POST['msg'];
+    if (isset($messageToSend) && preg_match("/[A-Za-z0-9]+/", $messageToSend) && count(explode(' ', $messageToSend)) == 2) {
 //    $_SESSION['test4'] = $_POST['msg'];
 //    unset($_SESSION['test4']);
 //    $_SESSION['test4'] = 'test1';
-    $database = new database('mysql-francois.alwaysdata.net','francois_oui','0621013579','francois_project');
-    $currentTopic = $database->getTopic(explode('/Topic/',$_SERVER['REQUEST_URI'])[1]);
-    $id = $currentTopic->getIdTopic();
-    if($database->getLastMessages($id) > 0){
+        $database = new database('mysql-francois.alwaysdata.net', 'francois_oui', '0621013579', 'francois_project');
+        $currentTopic = $database->getTopic(explode('/Topic/', $_SERVER['REQUEST_URI'])[1]);
+        $id = $currentTopic->getIdTopic();
+        if ($database->getLastMessages($id) > 0) {
 //        $database->newMessage($currentTopic);
-        $database->addContentMsg($database->getLastMessages($currentTopic->getIdTopic()), $_POST['msg']);
-    } else {
-        $database->newMessage($id);
-        $database->addContentMsg($database->getLastMessages($currentTopic->getIdTopic()), $_POST['msg']);
+            $database->addContentMsg($database->getLastMessages($currentTopic->getIdTopic()), $_POST['msg']);
+        } else {
+            $database->newMessage($id);
+            $database->addContentMsg($database->getLastMessages($currentTopic->getIdTopic()), $_POST['msg']);
 //        $database->addContentMsg($database->getLastMessages($currentTopic->getIdTopic()), $_POST['msg']);
-    }
+        }
 //    echo 'iojzadoijdzaiojziaodjzdaoizajoizajzioajaziojzdaojzaiozjo';
 //    header('Location : /Topic/12');
 //    exit();
+    } else {
+        return false;
+    }
 }
 
 ?>
