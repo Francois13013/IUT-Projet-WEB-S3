b996a563cbf47ec0e59313d59854ac89916e27d1
François <Francois.al-haddad-siderikoudis@etu.univ-amu.fr>
2019-10-27 11:16:10 +0100

    Creation d'un generateur de mot de passe robuste

--- Controllers/controllerForgetPassword.php
+++ Controllers/controllerForgetPassword.php
@@ -3,10 +3,18 @@
 require_once('../classes/Database.php');
 session_start();
 
+function RandPassword(){
+    $passgen = NULL;
+    for($i = 1; $i<10; ++$i) {
+        $passgen .= chr(rand(46, 125));
+    }
+    return $passgen;
+}
+
 $email = $_POST['email'];
 $databaseBaptiste = new database('mysql-baptistesevilla.alwaysdata.net', '189826_admin1', '0651196362', 'baptistesevilla_projetweb');
 if ($databaseBaptiste->CheckEmail($email) == 1) {
-    $newPass = rand(1000000, 100000000);
+    $newPass = RandPassword();
     mail($email, 'Changement de mot de passe', 'Voici votre nouveau mdp ' . $newPass);
     $databaseBaptiste->updatePassword($email,sha1($newPass));
 }
--- Views/viewForgetPassword.php
+++ Views/viewForgetPassword.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
     if(isset($_SESSION['MailSend']) && $_SESSION['MailSend'] == 'whynot'){ echo 'Mail envoyé si ' . " l'adresse" . ' email existe.';}
-?>
+    ?>
 <form action="../Controllers/controllerForgetPassword.php" method="POST">
     <input name='email' type='text'>
     <button>Envoyer</button>
