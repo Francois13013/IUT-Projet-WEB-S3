96a9879b027e33e9da9087f36614827301fa2f2e
François <Francois.al-haddad-siderikoudis@etu.univ-amu.fr>
2019-10-27 15:01:09 +0000

    Controller Admin User

---
+++ Views/viewAdmin.php
@@ -0,0 +1,28 @@
+<?php
+
+?>
+
+<div id="FindUser">
+    <p>Rechercher par</p>
+    <div>
+        <label for="inputTextAdmin">Email</label>
+        <input id="inputTextAdmin" type="text">
+        <label for="inputTextId">Id</label>
+        <input id="inputTextId" type="text">
+        <label for="inputTextPseudo">Pseudo</label>
+        <input id="inputTextPseudo" type="text">
+        <button type="submit"></button>
+    </div>
+    <div id="InfoUser">
+        <label for="inputTextAdmin">Email</label>
+        <input id="inputTextAdmin" type="text">
+        <label for="inputTextId">Id</label>
+        <input id="inputTextId" type="text">
+        <label for="inputTextPseudo">Pseudo</label>
+        <input id="inputTextPseudo" type="text">
+    </div>
+    <div>
+        <button type="submit">Modifier</button>
+        <button type="submit">Supprimer</button>
+    </div>
+</div>
